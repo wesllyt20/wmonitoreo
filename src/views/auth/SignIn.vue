8e3b35a2a5e03cae6ed8ea013870b67a92f724fb
wesllyt20 <raphael.09@hotmail.es>
2023-02-16 09:51:54 -0500

    test 16/02

--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -8,35 +8,14 @@
       @submit="onSubmitLogin"
       :validation-schema="login"
     >
-      <!--begin::Heading-->
-      <div class="text-center mb-10">
-        <!--begin::Title-->
-        <h1 class="text-dark mb-3">Sign In to Metronic</h1>
-        <!--end::Title-->
+    
 
-        <!--begin::Link-->
-        <div class="text-gray-400 fw-bold fs-4">
-          New Here?
-
-          <router-link to="/sign-up" class="link-primary fw-bolder">
-            Create an Account
-          </router-link>
-        </div>
-        <!--end::Link-->
-      </div>
-      <!--begin::Heading-->
-
-      <div class="mb-10 bg-light-info p-8 rounded">
-        <div class="text-info">
-          Use account <strong>admin@demo.com</strong> and password
-          <strong>demo</strong> to continue.
-        </div>
-      </div>
+      
 
       <!--begin::Input group-->
       <div class="fv-row mb-10">
         <!--begin::Label-->
-        <label class="form-label fs-6 fw-bolder text-dark">Email</label>
+        <label class="form-label fs-6 fw-bolder text-dark">Correo</label>
         <!--end::Label-->
 
         <!--begin::Input-->
@@ -61,13 +40,13 @@
         <div class="d-flex flex-stack mb-2">
           <!--begin::Label-->
           <label class="form-label fw-bolder text-dark fs-6 mb-0"
-            >Password</label
+            >Contraseña </label
           >
           <!--end::Label-->
 
           <!--begin::Link-->
           <router-link to="/password-reset" class="link-primary fs-6 fw-bolder">
-            Forgot Password ?
+            ¿Olvidó contraseña?
           </router-link>
           <!--end::Link-->
         </div>
@@ -98,61 +77,16 @@
           id="kt_sign_in_submit"
           class="btn btn-lg btn-primary w-100 mb-5"
         >
-          <span class="indicator-label"> Continue </span>
+          <span class="indicator-label"> Ingresar </span>
 
           <span class="indicator-progress">
-            Please wait...
+            Por favor espere...!
             <span
               class="spinner-border spinner-border-sm align-middle ms-2"
             ></span>
           </span>
         </button>
         <!--end::Submit button-->
-
-        <!--begin::Separator-->
-        <div class="text-center text-muted text-uppercase fw-bolder mb-5">
-          or
-        </div>
-        <!--end::Separator-->
-
-        <!--begin::Google link-->
-        <a
-          href="#"
-          class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5"
-        >
-          <img
-            alt="Logo"
-            src="media/svg/brand-logos/google-icon.svg"
-            class="h-20px me-3"
-          />
-          Continue with Google
-        </a>
-        <!--end::Google link-->
-
-        <!--begin::Google link-->
-        <a
-          href="#"
-          class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5"
-        >
-          <img
-            alt="Logo"
-            src="media/svg/brand-logos/facebook-4.svg"
-            class="h-20px me-3"
-          />
-          Continue with Facebook
-        </a>
-        <!--end::Google link-->
-
-        <!--begin::Google link-->
-        <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100">
-          <img
-            alt="Logo"
-            src="media/svg/brand-logos/apple-black.svg"
-            class="h-20px me-3"
-          />
-          Continue with Apple
-        </a>
-        <!--end::Google link-->
       </div>
       <!--end::Actions-->
     </Form>
