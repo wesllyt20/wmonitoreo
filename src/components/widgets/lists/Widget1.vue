fabc8ab77aa04cacef0b0688142008c85db70dba
wesllyt20 <raphael.09@hotmail.es>
2023-02-23 12:02:20 -0500

    22/02

--- src/components/widgets/lists/Widget1.vue
+++ src/components/widgets/lists/Widget1.vue
@@ -1,47 +1,7 @@
 <template>
   <!--begin::List Widget 1-->
   <div class="card" :class="widgetClasses">
-    <!--begin::Header-->
-    <div class="card-header border-0 pt-5">
-      <h1>Hello</h1>
-    </div>
-    <!--end::Header-->
-
-    <!--begin::Body-->
-    <div class="card-body pt-5">
-      <template v-for="(item, index) in items" :key="index">
-        <!--begin::Item-->
-        <div
-          :class="{ 'mb-7': items.length - 1 !== index }"
-          class="d-flex align-items-center"
-        >
-          <!--begin::Symbol-->
-          <div class="symbol symbol-50px me-5">
-            <span :class="`bg-light-${item.color}`" class="symbol-label">
-              <span
-                :class="`svg-icon-${item.color}`"
-                class="svg-icon svg-icon-2x"
-              >
-                <inline-svg :src="item.icon" />
-              </span>
-            </span>
-          </div>
-          <!--end::Symbol-->
-
-          <!--begin::Text-->
-          <div class="d-flex flex-column">
-            <a href="#" class="text-dark text-hover-primary fs-6 fw-bolder">{{
-              item.title
-            }}</a>
-
-            <span class="text-muted fw-bold">{{ item.description }}</span>
-          </div>
-          <!--end::Text-->
-        </div>
-        <!--end::Item-->
-      </template>
-    </div>
-    <!--end::Body-->
+    
   </div>
   <!--end::List Widget 1-->
 </template>
@@ -59,42 +19,12 @@ export default defineComponent({
     widgetClasses: String,
   },
   setup() {
-    const items = [
-      {
-        icon: "media/icons/duotune/abstract/abs027.svg",
-        color: "success",
-        title: "Project Briefing",
-        description: "Project Manager",
-      },
-      {
-        icon: "media/icons/duotune/art/art005.svg",
-        color: "warning",
-        title: "Concept Design",
-        description: "Art Director",
-      },
-      {
-        icon: "media/icons/duotune/communication/com012.svg",
-        color: "primary",
-        title: "Functional Logics",
-        description: "Lead Developer",
-      },
-      {
-        icon: "media/icons/duotune/coding/cod008.svg",
-        color: "danger",
-        title: "Development",
-        description: "DevOps",
-      },
-      {
-        icon: "media/icons/duotune/general/gen049.svg",
-        color: "info",
-        title: "Testing",
-        description: "QA Managers",
-      },
-    ];
 
-    return {
-      items,
-    };
+
+
+
+
+    
   },
 });
 </script>
---
+++ src/components/widgets/lists/timeline.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="timeline-label mt-7">
+    <div class="timeline-item" v-for="(event, index) in events" :key="index">
+      <div class="timeline-badge text-primary">
+        <i class="fa fa-genderless text-primary fs-1"></i>
+      </div>
+      <div class="timeline-item-content">
+        <h6 class="texw5" v-html="event.title"></h6>
+        <h8 class="texw5" v-html="event.subtitle"></h8>
+      </div>
+    </div>
+  </div>
+</template>
+  
+  <script>
+export default {
+  data() {
+    return {
+      events: [
+        {
+          title: "<strong>Lunes 19 de Agosto</strong>",
+          subtitle: "Reporte del Censis operativo",
+        },
+        {
+          title: "<strong>Martes 10 de Agosto</strong>",
+          subtitle: "<strong style=\"color: red;\">[URGENTE]</strong> Simulacro de repositorio caido"
+        },
+        {
+          title: "<strong>Miércoles 8 de Agosto</strong>",
+          subtitle: "Ataques a servidores de web CENVUL",
+        },
+        {
+          title: "<strong>Jueves 5 de Agosto</strong>",
+          subtitle: "Prueba de servidores CENCIS",
+        }
+      ],
+    };
+  },
+  methods: {
+    addEvent(event) {
+      this.events.push(event);
+    },
+  },
+};
+</script>
+  <style>
+.texw5 {
+  margin-left: 10px !important;
+}
+</style>
